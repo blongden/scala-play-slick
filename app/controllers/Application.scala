2da2ad344de1e065f3027b03832af870f355f502
Ben Longden <blongden@inviqa.com>
2015-10-27 17:47:03 +0000

    Refactor case class out into models package

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,28 +1,26 @@
 package controllers
 
 import javax.inject.Inject
-
 import play.api._
 import play.api.mvc._
 import slick.driver.JdbcProfile
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
-case class User(id: Int, email: String, password: String, fullname: String, isAdmin: Int)
+import models.{User => UserModel}
 
 class Application @Inject() (dbConfigProvider: db.slick.DatabaseConfigProvider) extends Controller {
   val dbConfig = dbConfigProvider.get[JdbcProfile]
 
   import dbConfig.driver.api._
 
-  private class UserTable(tag: Tag) extends Table[User](tag, "USER") {
+  private class UserTable(tag: Tag) extends Table[UserModel](tag, "USER") {
     def id = column[Int]("id", O.PrimaryKey)
     def email = column[String]("email")
     def password = column[String]("password")
     def fullname = column[String]("fullname")
     def isAdmin = column[Int]("isAdmin")
 
-    def * = (id, email, password, fullname, isAdmin) <> ((User.apply _).tupled, User.unapply)
+    def * = (id, email, password, fullname, isAdmin) <> ((UserModel.apply _).tupled, UserModel.unapply)
   }
 
   private val users = TableQuery[UserTable]
