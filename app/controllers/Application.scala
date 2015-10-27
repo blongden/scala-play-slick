7ff4e1bb32178b07b33add73798bd03d7791c223
Ben Longden <blongden@inviqa.com>
2015-10-27 17:38:00 +0000

    Refactoring out into case class

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,19 +8,21 @@ import slick.driver.JdbcProfile
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
+case class User(id: Int, email: String, password: String, fullname: String, isAdmin: Int)
+
 class Application @Inject() (dbConfigProvider: db.slick.DatabaseConfigProvider) extends Controller {
   val dbConfig = dbConfigProvider.get[JdbcProfile]
 
   import dbConfig.driver.api._
 
-  private class UserTable(tag: Tag) extends Table[(Int, String, String, String, Int)](tag, "USER") {
-    def id: Rep[Int] = column[Int]("id", O.PrimaryKey)
-    def email: Rep[String] = column[String]("email")
-    def password: Rep[String] = column[String]("password")
-    def fullname: Rep[String] = column[String]("fullname")
-    def isAdmin: Rep[Int] = column[Int]("isAdmin")
+  private class UserTable(tag: Tag) extends Table[User](tag, "USER") {
+    def id = column[Int]("id", O.PrimaryKey)
+    def email = column[String]("email")
+    def password = column[String]("password")
+    def fullname = column[String]("fullname")
+    def isAdmin = column[Int]("isAdmin")
 
-    def * = (id, email, password, fullname, isAdmin)
+    def * = (id, email, password, fullname, isAdmin) <> ((User.apply _).tupled, User.unapply)
   }
 
   private val users = TableQuery[UserTable]
