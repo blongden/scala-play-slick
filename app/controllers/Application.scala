d3ab612d7aa4540929c0b750c94513e219c1434f
Ben Longden <blongden@inviqa.com>
2015-10-28 15:38:48 +0000

    Added form and validation.
    F/E looks pretty slick...

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,15 +2,40 @@ package controllers
 
 import javax.inject.Inject
 import models.Users
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
 import play.api.mvc._
+import play.api.i18n._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-class Application @Inject() (userRep: Users) extends Controller {
-  def index = Action.async { implicit request =>
-    userRep.list().map(users => Ok(views.html.index(users)))
+class Application @Inject() (userRep: Users, val messagesApi: MessagesApi) extends Controller with I18nSupport {
+  val userForm: Form[CreateUserForm] = Form {
+    mapping(
+      "email" -> nonEmptyText,
+      "password" -> nonEmptyText,
+      "fullname" -> nonEmptyText,
+      "isAdmin" -> boolean
+    )(CreateUserForm.apply)(CreateUserForm.unapply)
   }
 
-  def addUser = Action.async {
-    userRep.add(java.util.UUID.randomUUID.toString, "bob@example.com", "password", "Bob Example", 0).map { _ => Redirect(routes.Application.index()) }
+  def index = Action.async { implicit request =>
+    userRep.list().map(users => Ok(views.html.index(userForm, users)))
+  }
+
+  def addUser = Action.async { implicit request =>
+    userForm.bindFromRequest.fold(
+      errorForm => {
+        userRep.list().map(users => Ok(views.html.index(errorForm, users)))
+      },
+
+      user => {
+        userRep.add(java.util.UUID.randomUUID.toString, user.email, user.password, user.fullname, if (user.isAdmin) 1 else 0).map { _ => Redirect(routes.Application.index()) }
+      }
+    )
+
   }
 }
+
+case class CreateUserForm(email: String, password: String, fullname: String, isAdmin: Boolean)
