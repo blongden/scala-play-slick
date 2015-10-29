37f7eed2044282b9c914d0bd45b1eca1eda29034
richardmiller <rmiller@inviqa.com>
2015-10-29 13:45:04 +0000

    Remove password from edit user form. Fixes #6

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,67 +11,66 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class Application @Inject() (userRep: Users, val messagesApi: MessagesApi) extends Controller with I18nSupport {
-  val userForm: Form[CreateUserForm] = Form {
+  val createForm: Form[CreateUser] = Form {
     mapping(
       "email" -> nonEmptyText,
       "password" -> nonEmptyText,
       "fullname" -> nonEmptyText,
       "isAdmin" -> boolean
-    )(CreateUserForm.apply)(CreateUserForm.unapply)
+    )(CreateUser.apply)(CreateUser.unapply)
   }
 
-  def index = Action.async { implicit request =>
-    userRep.list().map(users => Ok(views.html.index(userForm, users)))
+  val editForm: Form[EditUser] = Form {
+    mapping(
+      "email" -> nonEmptyText,
+      "fullname" -> nonEmptyText,
+      "isAdmin" -> boolean
+    )(EditUser.apply)(EditUser.unapply)
   }
+
+  def index = Action.async { userRep.list().map(users => Ok(views.html.index(createForm, users))) }
 
   def addUser = Action.async { implicit request =>
-    userForm.bindFromRequest.fold(
-      errorForm => {
-        userRep.list().map(users => Ok(views.html.index(errorForm, users)))
-      },
-
-      user => {
+    createForm.bindFromRequest.fold(
+      errorForm => userRep.list().map(users => Ok(views.html.index(errorForm, users))),
+      formData => {
         userRep.add(
           User(
             java.util.UUID.randomUUID.toString,
-            user.email,
-            user.password,
-            user.fullname,
-            if (user.isAdmin) 1 else 0
-        )).map { _ => Redirect(routes.Application.index()) }
+            formData.email,
+            formData.password,
+            formData.fullname,
+            if (formData.isAdmin) 1 else 0
+        )).map(redirectToIndex)
       }
     )
   }
 
-  def editUser(id: String) = Action.async { implicit request =>
+  def editUser(id: String) = Action.async {
     userRep.findById(id).map {
-      case Some(u) => Ok(views.html.edit(id, userForm.fill(CreateUserForm(u.email, u.password, u.fullname, u.isAdmin == 1))))
-      case None => NotFound(s"No user was found for id: $id")
+      case Some(u) => Ok(views.html.edit(id, editForm.fill(EditUser(u.email, u.fullname, u.isAdmin == 1))))
+      case None => userNotFound(id)
     }
   }
 
   def saveUser(id: String) = Action.async { implicit request =>
-    userForm.bindFromRequest.fold(
-      errorForm => {
-        Future.successful(Ok(views.html.edit(id, errorForm)))
-      },
-
-      user => {
-        userRep.update(
-          User(
-            id,
-            user.email,
-            user.password,
-            user.fullname,
-            if (user.isAdmin) 1 else 0
-          )).map { _ => Redirect(routes.Application.index()) }
-      }
+    editForm.bindFromRequest.fold(
+      errorForm => Future.successful(Ok(views.html.edit(id, errorForm))),
+      formData => {
+        userRep.findById(id).flatMap {
+          case None => Future.successful(userNotFound(id))
+          case Some(existingUser) => userRep.update(User(id, formData.email, existingUser.password, formData.fullname, if (formData.isAdmin) 1 else 0)).map(redirectToIndex)
+          }
+        }
     )
   }
 
-  def deleteUser(id: String) = Action.async {
-    userRep.delete(id).map { _ => Redirect(routes.Application.index()) }
-  }
+  def deleteUser(id: String) = Action.async {userRep.delete(id) map redirectToIndex}
+
+  private def userNotFound(id: String) = NotFound(s"No user was found for id: $id")
+
+  private def redirectToIndex(a: Any) = Redirect(routes.Application.index())
 }
 
-case class CreateUserForm(email: String, password: String, fullname: String, isAdmin: Boolean)
+case class CreateUser(email: String, password: String, fullname: String, isAdmin: Boolean)
+case class EditUser(email: String, fullname: String, isAdmin: Boolean)
